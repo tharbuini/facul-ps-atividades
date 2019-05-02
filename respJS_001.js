4d6c359bb0e5bf6d62b5c61e321152080c312b21
thiguiNz <48720971+thiguiNz@users.noreply.github.com>
2019-05-02 09:45:56 -0300

    Update respJS_001.js

--- respJS_001.js
+++ respJS_001.js
@@ -3,7 +3,7 @@ function Q1() {
     var x = Number.parseInt(document.getElementById('x').value);
     var y = Number.parseInt(document.getElementById('y').value);
     // Segue aqui seu código
-    console.log(‘X / Y:  ‘ + x + ‘ / ‘ + y);
+    console.log(‘X / Y = ‘ + x + ‘ / ‘ + y);
     console.log(‘Quociente = ‘ + Math.floor(x / y));
     console.log(‘Resto = ‘ + (x % y));
 }
