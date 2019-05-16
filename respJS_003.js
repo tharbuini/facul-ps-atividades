c65172e22da76ed22e92c50825f67b7efb1383f0
aluno <aluno@lab3q105-p5qc01.ufu.br>
2019-05-16 10:42:38 -0300

    s

---
+++ respJS_003.js
@@ -0,0 +1,10 @@
+var pares = [];
+// Crie aqui suas funções de apoio
+
+function Q1() {
+    console.log('Q1');
+    var aux = document.getElementById('numerosQ1').text;
+    // Segue aqui seu código
+
+
+}
