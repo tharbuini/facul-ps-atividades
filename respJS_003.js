0e6cddc7c9cb19827fbe0a52ac8f94e19b3e4915
aluno <aluno@lab3q105-p5qc01.ufu.br>
2019-05-16 11:12:06 -0300

    a

--- respJS_003.js
+++ respJS_003.js
@@ -1,15 +1,31 @@
-var pares = [];
-// Crie aqui suas funções de apoio
+
+
 
 function Q1() {
+
     console.log('Q1');
-    var aux = document.getElementById('numerosQ1').text;
+    var pares_= [];
+    var aux = document.getElementById('numerosQ1').value;
     // Segue aqui seu código
 
-    for (let i = 0; i <= Number.parseInt(aux); i+= 2) {
+    for (let i = 0; i <= aux; i+= 2) {
     pares_.push(i);
     }
     console.log(pares_)
     document.getElementById('RQ1').innerHTML = pares_;
+
+}
+function Q2() {
+
+    console.log('Q2');
+    var primo= [];
+    var aux = document.getElementById('numerosQ2').value;
+    // Segue aqui seu código
+
+    for (let i = 0; i <= aux; i+= 2) {
+    primo.push(i);
+    }
+    console.log(primo)
+    document.getElementById('RQ2').innerHTML = primo;
 
 }
